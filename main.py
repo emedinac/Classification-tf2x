c3484d174c71e462dd6380dbfdb2915b1f291c3c
edgarmedina1801 <edgarmedina1801@gmail.com>
2021-04-24 14:15:18 +0200

    Running a more robust network with data augmentation, demo is working well.

--- main.py
+++ main.py
@@ -1,46 +1,56 @@
 import tensorflow as tf
 import tensorflow_datasets as tfds
 
-from tensorflow.keras.layers import Dense, Flatten, Conv2D
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, GlobalAveragePooling2D
 from tensorflow.keras import Model
 
 import dataset
 
+test_summary_writer = tf.summary.create_file_writer('logdir')
+
 # Data augmentation here may increase the accuracy in the training results.
 def Augmentation(image):
+    image = tf.image.resize(image,(224,224))
     image = tf.image.convert_image_dtype(image, tf.float32)
+    image = tf.image.flip_left_right(image)
+    image = tf.image.flip_up_down(image)
+    # image = tf.image.resize_with_crop_or_pad(image,20,20)
+    # image = tf.image.random_contrast(image, lower=0.0, upper=1.0)
     return image
-    tf.image.flip_left_right(image)
-    tf.image.random_contrast(image, lower=0.0, upper=1.0)
 
 # Load my dataset here
 # This functions were created in order to follow the full-pipeline format from TFDS-nightly.
-train_ds = tfds.load("dataset", split='train', shuffle_files=True, batch_size=32)
-test_ds = tfds.load("dataset", split='test', shuffle_files=True, batch_size=32)
+train_ds = tfds.load("dataset", split='train', shuffle_files=True, batch_size=8)
+test_ds = tfds.load("dataset", split='test', shuffle_files=True, batch_size=8)
 train_ds = train_ds.map(lambda data: (Augmentation(data["image"]), data["label"])).cache()
 test_ds = test_ds.map(lambda data: (Augmentation(data["image"]), data["label"])).cache()
 
-class MyModel(Model):
-  def __init__(self):
-    super(MyModel, self).__init__()
-    self.conv1 = Conv2D(32, 3, activation='relu')
-    self.flatten = Flatten()
-    self.d1 = Dense(128, activation='relu')
-    self.d2 = Dense(10)
+# A default model just for testing.
+# class MyModel(Model):
+#   def __init__(self):
+#     super(MyModel, self).__init__()
+#     self.conv1 = Conv2D(32, 3, activation='relu')
 
-  def call(self, x):
-    x = self.conv1(x)
-    x = self.flatten(x)
-    x = self.d1(x)
-    return self.d2(x)
+#     self.flatten = Flatten()
+#     self.d1 = Dense(128, activation='relu')
+#     self.d2 = Dense(10)
 
-test_summary_writer = tf.summary.create_file_writer('logdir')
-
+#   def call(self, x):
+#     x = self.conv1(x)
+#     x = self.flatten(x)
+#     x = self.d1(x)
+#     return self.d2(x)
 # Create an instance of the model
-model = MyModel()
+# model = MyModel()
+
+base_model = tf.keras.applications.EfficientNetB0(include_top=False, weights="imagenet", classes=2)
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+predictions = Dense(2, activation='softmax')(x)
+model = Model(inputs=base_model.input, outputs=predictions)
+
 
 loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-
 optimizer = tf.keras.optimizers.Adam()
 
 train_loss = tf.keras.metrics.Mean(name='train_loss')
@@ -49,10 +59,8 @@ train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy
 test_loss = tf.keras.metrics.Mean(name='test_loss')
 test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
 
-
-
 @tf.function
-def train_step(images, labels):
+def train_stage(images, labels):
     with tf.GradientTape() as tape:
         predictions = model(images, training=True)
         loss = loss_object(labels, predictions)
@@ -62,9 +70,8 @@ def train_step(images, labels):
     train_loss(loss)
     train_accuracy(labels, predictions)
 
-
 @tf.function
-def test_step(images, labels):
+def test_stage(images, labels):
     # training=False is only needed if there are layers with different
     # behavior during training versus inference (e.g. Dropout).
     predictions = model(images, training=False)
@@ -72,9 +79,9 @@ def test_step(images, labels):
 
     test_loss(t_loss)
     test_accuracy(labels, predictions)
-EPOCHS = 5
 
-for epoch in range(EPOCHS):
+
+for epoch in range(20):
     # Reset the metrics at the start of the next epoch
     train_loss.reset_states()
     train_accuracy.reset_states()
@@ -82,10 +89,10 @@ for epoch in range(EPOCHS):
     test_accuracy.reset_states()
 
     for image, label in train_ds:
-        train_step(image, label)
+        train_stage(image, label)
 
     for image, label in test_ds:
-        test_step(image, label)
+        test_stage(image, label)
 
     with test_summary_writer.as_default():
         tf.summary.scalar('train_loss',train_loss.result(),epoch)
